fb8e895b7f24e0f6a21d07fef3105e0a50665bfb
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-10-18 14:31:34 +0530

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Sidebar } from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Sidebar></Sidebar>
         
       </header>
     </div>
