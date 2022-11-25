ab09c5f2df8d193b20e9e8259585389d4079dba9
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-11-25 23:53:54 +0530

    cart added

--- src/components/TodoCart.jsx
+++ src/components/TodoCart.jsx
@@ -1,5 +1,20 @@
+import { Box, Button, Flex, Heading,Tag,TagLabel,TagLeftIcon,Text } from "@chakra-ui/react";
 import React from "react";
-
+import { CgHashtag } from "react-icons/cg";
 export const TodoCart = () => {
-  return <div>TodoCart</div>;
+  return <Box>
+    <Text fontSize={"25px"}>The Todo Content</Text>
+    <Text fontSize={"25px"}>20/20/25</Text>
+    <Button variant={"ghost"}> Progress</Button>
+    <Flex>
+      <Tag size={"lg"} borderRadius={"full"}>
+        <Box background={"#6136FA"}fontWeight={900} padding={"5px"} >
+          <CgHashtag color="white"  ></CgHashtag>
+        </Box>
+      
+      <TagLabel>Ccfdsyan</TagLabel>
+      </Tag>
+    </Flex>
+
+  </Box>;
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { TodoCart } from "../components/TodoCart";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return <div>
+    <TodoCart></TodoCart>
+  </div>;
 };
